b46128167e20b330ff5c426bcfdb3151a6c1a599
Samuel Utbult <samuel.utbult@oborgen.se>
2021-03-18 14:24:58 +0100

    Add more documentation to UniqueRandom

--- packages/nano_var/test/src/utils/unique_random.dart
+++ packages/nano_var/test/src/utils/unique_random.dart
@@ -4,10 +4,17 @@ import 'dart:math';
 /// which defaults to `100`.
 /// The maximum value is increased by one each time `next` is called.
 class UniqueRandom {
+  /// The current maximum value.
   int _max;
+
+  /// The values that already has been generated.
   final List<int> _previousValues;
+
+  /// A [Random] instance used to generate values.
   final Random _random;
 
+  /// Creates a [UniqueRandom] with an initial maximum value `max`, which
+  /// defaults to `100`.
   UniqueRandom([int max = 100])
       : _max = max,
         _previousValues = [],
