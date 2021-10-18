77396d0d3cd68905a6badbb5e2e547b2afd9d14f
Samuel Utbult <samuel.utbult@oborgen.se>
2021-10-18 20:24:51 +0200

    Move all logic from NanoRead to NanoVar and make NanoRead abstract
    Refs #19

--- packages/nano_var/lib/src/nano_var.dart
+++ packages/nano_var/lib/src/nano_var.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 import 'nano_read.dart';
 
 /// Instances of this class holds a value of type `T` and it's possible to
@@ -5,12 +7,63 @@ import 'nano_read.dart';
 ///
 /// The value can be both get and set.
 class NanoVar<T> extends NanoRead<T> {
-  /// Creates a new [NanoVar] with a given `initialValue`.
-  NanoVar(T initialValue) : super(initialValue);
+  /// The value held by the instance.
+  T _value;
+
+  /// A list of callbacks that are called each time a change to the value
+  /// occurs, i.e. the value's subscribers.
+  final List<void Function(T, T)> _subscribers;
+
+  /// Returns the current number of subscribers.
+  ///
+  /// This getter is only supposed to be used in test cases.
+  @visibleForTesting
+  int get subscribersCount => _subscribers.length;
+
+  /// Creates a new [NanoRead] with a given `initialValue`.
+  NanoVar(T initialValue)
+      : _value = initialValue,
+        _subscribers = [];
+
+  /// Gets the current value.
+  T get value {
+    return _value;
+  }
+
+  /// Subscribes to changes to the value, which means `callback` is called each
+  /// time a change occurs.
+  ///
+  /// The function returns a function that when called unsubscribes to the
+  /// value, which means `callback` will never be called again by the class.
+  void Function() subscribe(void Function(T oldValue, T newValue) callback) {
+    // Add the given callback to the list of subscribers.
+    _subscribers.add(callback);
+
+    // Return a callback that can be used to unsubscribe to the variable.
+    return () {
+      // Remove the given callback.
+      // This works since Dart compares the memory addresses of the callbacks
+      // and thereby can remove the given callback.
+      _subscribers.remove(callback);
+    };
+  }
 
   /// Sets the current value and notifies all subscribers.
   set value(T newValue) {
-    // Call change to set the given value.
-    change(newValue);
+    // Store the current value as the old value.
+    final oldValue = _value;
+
+    // Check if the new value are the same as the current value.
+    // If so, nothing is done.
+    if (newValue != oldValue) {
+      // Set newValue as the new value.
+      _value = newValue;
+
+      // Call each subscriber.
+      _subscribers.forEach((callback) {
+        // Call callback on the current subscriber with the old and new values.
+        callback(oldValue, newValue);
+      });
+    }
   }
 }
