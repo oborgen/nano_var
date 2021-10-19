96db0c357d16cc5351557d86372c133918f0122e
Samuel Utbult <samuel.utbult@oborgen.se>
2021-10-19 17:36:47 +0200

    Add FunctorNanoRead
    Refs #19

--- packages/nano_var/lib/nano_var.dart
+++ packages/nano_var/lib/nano_var.dart
@@ -1,3 +1,4 @@
+export 'src/functor_nano_read.dart';
 export 'src/nano_read.dart';
 export 'src/nano_read_subscribe_callback.dart';
 export 'src/nano_var.dart';
---
+++ packages/nano_var/lib/src/functor_nano_read.dart
@@ -0,0 +1,126 @@
+import 'package:nano_var/src/nano_channel.dart';
+
+import 'nano_read.dart';
+
+/// An extension of [NanoRead] that makes [NanoRead] act as a functor,
+/// i.e. the method `map` can be called with a callback, which returns a new
+/// [NanoRead] instance containing the values returned by the given callback,
+/// updated as the original [NanoRead] updates.
+extension FunctorNanoRead<T> on NanoRead<T> {
+  /// Maps this [NanoRead] with the given callback `mapper` and returns a new
+  /// [NanoRead] instance as a result.
+  NanoRead<S> map<S>(S Function(T) mapper) {
+    // Create and return a _FunctorNanoRead, which handles all logic.
+    return _FunctorNanoRead(this, mapper);
+  }
+}
+
+/// A [NanoRead] instance that handles the functor logic.
+class _FunctorNanoRead<T, S> implements NanoRead<S> {
+  /// The original [NanoRead] to retrieve arguments to `mapper` from.
+  final NanoRead<T> source;
+
+  /// A function used to call each value from `source` with to create new
+  /// values.
+  final S Function(T) mapper;
+
+  /// A [NanoChannel] to handle all subscriptions to this [_FunctorNanoRead]
+  /// instance.
+  final NanoChannel<S> _channel;
+
+  /// The unsubscribe callback for `source`.
+  ///
+  /// The value is null when this [_FunctorNanoRead] has no subscribers.
+  void Function()? _unsubscribeSource;
+
+  /// The most recently retrieved value from `source`.
+  late T _sourceValue;
+
+  /// The most recently value calculated by `mapper.
+  late S _resultValue;
+
+  _FunctorNanoRead(
+    this.source,
+    this.mapper,
+  ) : _channel = NanoChannel() {
+    // Initialize _sourceValue with source's current value.
+    _sourceValue = source.value;
+
+    // Initialize _resultValue with _sourceValue.
+    _resultValue = mapper(_sourceValue);
+  }
+
+  @override
+  void Function() subscribe(callback) {
+    // Call _updateSubscribeToSource to subscribe to source if necessary.
+    _updateSubscribeToSource();
+
+    // Make the callback subscribe to _channel.
+    final unsubscribe = _channel.subscribe(callback);
+
+    return () {
+      // Call unsubscribe to unsubscribe from _channel.
+      unsubscribe();
+
+      // Call _updateUnsubscribeToSource to unsubscribe to source if necessary.
+      _updateUnsubscribeToSource();
+    };
+  }
+
+  @override
+  S get value {
+    // Call _update to update _sourceValue and _resultValue if necessary.
+    _update();
+
+    // Return _resultValue.
+    return _resultValue;
+  }
+
+  void _update() {
+    // Get source's current value.
+    final sourceValue = source.value;
+
+    // Check if source's value has changed.
+    if (_sourceValue != sourceValue) {
+      // Assign the new value to _sourceValue.
+      _sourceValue = sourceValue;
+
+      // Call mapper with the new value and assign it to _resultValue.
+      _resultValue = mapper(sourceValue);
+    }
+  }
+
+  void _updateSubscribeToSource() {
+    // Check if source already has been subscribed to.
+    if (_unsubscribeSource == null) {
+      // Subscribe to source as no subscription already exists and assign the
+      // unsubscribe function to _unsubscribeSource.
+      _unsubscribeSource = source.subscribe((_, __) {
+        // Store the current _resultValue.
+        final oldResultValue = _resultValue;
+
+        // Call _update to update _sourceValue and _resultValue.
+        _update();
+
+        // Emit a new pair on _channel with the previously stored _resultValue
+        // and the newly calculated _resultValue.
+        _channel.emit(oldResultValue, _resultValue);
+      });
+    }
+  }
+
+  void _updateUnsubscribeToSource() {
+    // Get _unsubscribeSource.
+    final unsubscribeSource = _unsubscribeSource;
+
+    // Check if the subscribe count is zero and if _unsubscribeSource is not
+    // null.
+    if (_channel.subscribersCount == 0 && unsubscribeSource != null) {
+      // Unsubscribe to source.
+      unsubscribeSource();
+
+      // Assign null to _unsubscribeSource.
+      _unsubscribeSource = null;
+    }
+  }
+}
